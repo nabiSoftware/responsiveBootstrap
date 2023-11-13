ad5455cd127a5557567e1fd6abd4b00c16aefa6c
nabiSoftware huseynov <huseynovnabi2002+demo@gmail.com>
2023-11-13 13:39:48 +0000

    changes

--- responsiveBootstrap/app.js
+++ responsiveBootstrap/app.js
@@ -1,4 +1,8 @@
 "use strict";
-$(document).ready(function () {
 
+$(document).ready(function () {
+    $("#hamburgerMenu").click(function () {
+        $("nav a").slideToggle();
+        $(this).toggleClass("fa-bars fa-times");
+    });
 });
