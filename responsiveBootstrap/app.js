56b69c2b506e023cd562f075fde983d1c62559ed
nabiSoftware huseynov <huseynovnabi2002+demo@gmail.com>
2023-11-13 21:46:10 +0000

    appJs

--- responsiveBootstrap/app.js
+++ responsiveBootstrap/app.js
@@ -2,14 +2,14 @@
 
 $(document).ready(function () {
     $("#hamburgerMenu").click(function () {
-        $("nav a").slideToggle();
-        $(this).toggleClass("fa-bars fa-times");
+        $("nav a").slideToggle(); // linklerin gizledilib ve ya gorsedilmesi
+        $(this).toggleClass("fa-bars fa-times"); // icon change
     });
 
 
     $("nav a").click(function () {
-        $("nav a").slideUp();
-        $("#hamburgerMenu").removeClass("fa-times").addClass("fa-bars");
+        $("nav a").slideUp(); // linklerin yuxariya dogru gizledilmesi
+        $("#hamburgerMenu").removeClass("fa-times").addClass("fa-bars"); // cancel iconun legv edib menu iconun elave edir 
     });
 
 });
